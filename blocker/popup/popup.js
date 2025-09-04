efdc7705e7ea01071538566104718fbae27f78be
Roo10iuM <danil5400000@gmail.com>
2025-09-04 12:18:48 +0300

    first try

---
+++ blocker/popup/popup.js
@@ -0,0 +1,21 @@
+//interface
+
+function setInterface(state) {
+    switch (state) {
+        case "ON":
+            
+            break;
+    
+        case "OFF":
+
+            break;
+    }
+}
+
+window.onload = () => {
+    const btn = document.getElementById("submit")
+    btn.onclick = () => {
+        chrome.runtime
+    }
+}
+
